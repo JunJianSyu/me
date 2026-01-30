23db65bce531c92e54d60210b144c381b262aa57
JunJianSyu <junjian1992@gmail.com>
2026-01-30 10:47:57 +0800

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export type {
+  Post,
+  PostFrontMatter,
+  PostListItem,
+  ChapterItem,
+  SeriesFrontMatter,
+  SeriesItem,
+  Series,
+  Chapter,
+  ContentItem,
+} from './post';
+export type { Category } from './category';
+export type { Tag } from './tag';
+export type { Page, PageFrontMatter } from './page';
+export type { SiteConfig, NavItem, SocialLinks } from './config';
